3d6fce7c82e25cbc09df115d99e17b8e693bd420
stanou <stan.demontmarin@gmail.com>
2023-08-28 12:42:27 +0200

    2nd exo

---
+++ J1/Exercice-Distributeur-de-boissons.md
@@ -0,0 +1,15 @@
+# Exercice - Distributeur de boissons
+
+Dans cet exercice, vous allez créer un programme simulant un distributeur automatique de boissons. Le distributeur propose 5 boissons différentes et le client peut choisir la boisson souhaitée en entrant son numéro (de 1 à 5). Après le choix de la boisson, le client doit entrer le montant de l'argent qu'il insère dans la machine. Si le montant est suffisant, la machine doit afficher un message indiquant que la boisson est en cours de distribution et rendre la monnaie si nécessaire. Sinon, la machine doit afficher un message indiquant que le montant est insuffisant et donner la possibilité au client de réessayer ou d'annuler l'opération.
+
+Pour cet exercice, vous devez utiliser les instructions conditionnelles (if, else if et else) ainsi que les boucles (for, while, do/while) et les contrôles de flux avancés (break et continue) pour gérer le fonctionnement du distributeur automatique.
+
+Instructions:
+
+1. Afficher le menu des boissons disponibles avec leur prix et leur numéro correspondant.
+2. Demander au client de choisir une boisson en entrant son numéro.
+3. Vérifier si le numéro entré est valide. Si ce n'est pas le cas, redemander au client d'entrer un numéro valide.
+4. Demander au client d'entrer le montant de l'argent qu'il insère dans la machine.
+5. Vérifier si le montant est suffisant pour acheter la boisson sélectionnée. Si c'est suffisant, afficher un message indiquant que la boisson est en cours de distribution et rendre la monnaie si nécessaire.
+6. Si le montant est insuffisant, donner deux options au client : réessayer avec un nouveau montant ou annuler l'opération.
+7. Répétez les étapes 4 à 6 jusqu'à ce que le client ait choisi d'annuler l'opération ou que la boisson ait été distribuée.
---
+++ J1/exo2.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+
+void display_menu()
+{
+    std::cout << "Beverages" << std::endl;
+    std::cout << "1 - coffee: 1.5€" << std::endl;
+    std::cout << "2 - coca: 2€" << std::endl;
+    std::cout << "3 - fanta: 2€" << std::endl;
+    std::cout << "4 - redbull: 2€" << std::endl;
+    std::cout << "5 - Beer: 5€" << std::endl;
+}
+
+int get_beverage()
+{
+    int choice;
+    std::cout << "Enter the number of the desired beverage" << std::endl;
+    std::cin >> choice;
+    return choice;
+    
+}
+
+float get_money()
+{
+    float money;
+    std::cin >> money;
+    return money;
+}
+
+int check_money(float money, float price)
+{
+    float remain;
+    remain = 0;
+    int new_choice;
+    if (money >= price)
+    {
+        remain = money - price;
+        if (remain > 0)
+        {
+            std::cout << "Thank you for your purchase" << std::endl;
+            std::cout << "Your beverage is being prepared" << std::endl;
+            std::cout << "Do not forget to get your money back " << remain << "€" << std::endl;
+            return 0;
+        }
+        else
+        {
+            std::cout << "Thank you for your purchase" << std::endl;
+            std::cout << "Your beverage is being prepared" << std::endl;
+            return 0;
+        }
+    }
+    else
+    {
+        std::cout << "Press 1 to enter a new amount" << std::endl;
+        std::cout << "Press 2 to cancel your purchase" << std::endl;
+        std::cin >> new_choice;
+        if (new_choice == 1)
+        {
+            std::cout << "Please enter a new amount" << std::endl;
+            money = get_money();
+
+            check_money(money, price);
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    return 0;
+    
+}
+
+int main()
+{
+    int choice;
+    float prices[5] = {1.5, 2, 2, 2, 5};
+    float price;
+    float money_paid;
+    int result;
+    while (true)
+    {
+        display_menu();
+        choice = get_beverage();
+        if (choice < 1 || choice > 6)
+        {
+            std::cout << "Pick an available beverage" << std::endl;
+        }
+        else
+        {
+            std::cout << "You picked number: " << choice << std::endl;
+            std::cout << "It costs: " << prices[choice - 1] << " €"<< std::endl;
+            std:: cout << "Please enter the desired amount" << std::endl;
+            money_paid = get_money();
+            result = check_money(money_paid, prices[choice - 1]);
+            if (result == 0 || result == -1)
+            {
+                break;
+            }
+
+            
+        }
+        
+    }
+    
+    return 0;
+}
