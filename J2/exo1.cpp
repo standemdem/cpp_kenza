b469b3bea21c1a116363c0e194ab1e7b4d2bed4c
stanou <stan.demontmarin@gmail.com>
2023-08-29 10:59:58 +0200

    added control structure

--- J2/exo1.cpp
+++ J2/exo1.cpp
@@ -2,6 +2,8 @@
 // #include <cctype>
 #include <string>
 #include <algorithm>
+#include <limits>
+#include <ios>
 
 using namespace std;
 
@@ -31,60 +33,72 @@ int main()
     {        
         cin >> choice;
         int longueur = texte.length();
-        switch (choice)
-        {
-        case 1:
-            system("clear");
-            display_menu();
-            cout << texte << endl;
-            break;
-        case 2:
-            system("clear");
-            display_menu();
-            cout << "Length of text: " << longueur << endl;
-            break;
-        case 3:
-            system("clear");
-            display_menu();
-            cout << "5th char: " << texte[4] << endl;
 
-            break;
-        case 4:
-            system("clear");
-            display_menu();
-            texte.replace(34, 8, "intéressante");
-            cout << "new phrase: " << texte << endl;
-            break;
-        case 5:
-            system("clear");
-            display_menu();
-            reverse(texte.begin(), texte.end());
-            cout << "Reversed phrase" << endl;
-            cout << texte << endl;
-            break;
-        case 6:
-            system("clear");
-            display_menu();
-            for (int i = 0; i < texte.length(); i++)
+        if (std::cin.fail()) {
+        std::cout << "Error: Invalid input. Please enter an integer." << std::endl;
+
+        // Clear the fail state of the cin stream and ignore any remaining characters in the input buffer
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }else{
+            switch (choice)
             {
-                texte[i] = toupper(texte[i]);
+            case 1:
+                system("clear");
+                display_menu();
+                cout << texte << endl;
+                break;
+            case 2:
+                system("clear");
+                display_menu();
+                cout << "Length of text: " << longueur << endl;
+                break;
+            case 3:
+                system("clear");
+                display_menu();
+                cout << "5th char: " << texte[4] << endl;
+
+                break;
+            case 4:
+                system("clear");
+                display_menu();
+                texte.replace(34, 8, "intéressante");
+                cout << "new phrase: " << texte << endl;
+                break;
+            case 5:
+                system("clear");
+                display_menu();
+                reverse(texte.begin(), texte.end());
+                cout << "Reversed phrase" << endl;
+                cout << texte << endl;
+                break;
+            case 6:
+                system("clear");
+                display_menu();
+                for (int i = 0; i < texte.length(); i++)
+                {
+                    texte[i] = toupper(texte[i]);
+                }
+                cout << "phrase en MAJ: "<< endl;
+                cout << texte << endl;
+                break;
+            case 7:
+                system("clear");
+                display_menu();
+                for (int i = 0; i < texte.length(); i++)
+                {
+                    texte[i] = tolower(texte[i]);
+                }
+                cout << "phrase en MIN: "<< endl;
+                cout << texte << endl;
+                break;
+            default:
+                cout << "Don't break my balls with your silly attempts" << endl;
+                break;
             }
-            cout << "phrase en MAJ: "<< endl;
-            cout << texte << endl;
-            break;
-        case 7:
-            system("clear");
-            display_menu();
-            for (int i = 0; i < texte.length(); i++)
-            {
-                texte[i] = tolower(texte[i]);
-            }
-            cout << "phrase en MIN: "<< endl;
-            cout << texte << endl;
-            break;
-        default:
-            break;
         }
     }
+        
+  
 
 }
