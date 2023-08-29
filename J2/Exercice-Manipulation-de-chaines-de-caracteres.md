70afcd10ca43ac87e2909553fda19b62bbdddd8c
stanou <stan.demontmarin@gmail.com>
2023-08-29 10:35:34 +0200

    exo1 day 2

---
+++ J2/Exercice-Manipulation-de-chaines-de-caracteres.md
@@ -0,0 +1,17 @@
+# Exercice - Manipulation de chaînes de caractères
+
+Exercice : Manipulation de chaînes de caractères en C++
+
+Objectif : Dans cet exercice, vous allez devoir manipuler et travailler avec des chaînes de caractères en utilisant les fonctions et méthodes de la bibliothèque standard de C++.
+
+Consignes :
+
+1. Créez une chaîne de caractères nommée `texte` contenant la phrase suivante : "La programmation en C++ est très amusante !".
+2. Stocker la longueur de la chaîne de caractères `texte` dans une variable `longueur`.
+3. Affichez le cinquième caractère de la chaîne `texte` .
+4. Remplacez le mot "amusante" par "intéressante".
+5. Inversez l'ordre des caractères de la chaîne `texte` en utilisant la méthode `reverse()`.
+6. Convertissez tous les caractères de la chaîne `texte` en majuscules. 
+7. Affichez la chaîne `texte` modifiée à l'écran.
+
+Remarque : N'oubliez pas d'inclure les bibliothèques `<string>`, `<algorithm>` et `<iostream>` pour pouvoir utiliser les fonctions et méthodes nécessaires pour cet exercice.
---
+++ J2/exo1.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+// #include <cctype>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+void display_menu()
+{
+    cout << "**********************************************" << endl;
+    cout << "Choose what you want to do" << endl;
+    cout << "[1] -  display phrase" << endl;
+    cout << "[2] -  display length of phrase" << endl;
+    cout << "[3] -  display 5th char" << endl;
+    cout << "[4] -  replace last word and display" << endl;
+    cout << "[5] -  display phrase inverted" << endl;
+    cout << "[6] -  convert phrase to maj and display" << endl;
+    cout << "[7] -  convert phrase to min and display" << endl;
+    cout << "**********************************************" << endl;
+
+}
+
+int main()
+{
+    system("chcp 65001");
+
+    string texte = "La programmation en C++ est très amusante !";
+    int choice;
+    display_menu();
+    while(true)
+    {        
+        cin >> choice;
+        int longueur = texte.length();
+        switch (choice)
+        {
+        case 1:
+            system("clear");
+            display_menu();
+            cout << texte << endl;
+            break;
+        case 2:
+            system("clear");
+            display_menu();
+            cout << "Length of text: " << longueur << endl;
+            break;
+        case 3:
+            system("clear");
+            display_menu();
+            cout << "5th char: " << texte[4] << endl;
+
+            break;
+        case 4:
+            system("clear");
+            display_menu();
+            texte.replace(34, 8, "intéressante");
+            cout << "new phrase: " << texte << endl;
+            break;
+        case 5:
+            system("clear");
+            display_menu();
+            reverse(texte.begin(), texte.end());
+            cout << "Reversed phrase" << endl;
+            cout << texte << endl;
+            break;
+        case 6:
+            system("clear");
+            display_menu();
+            for (int i = 0; i < texte.length(); i++)
+            {
+                texte[i] = toupper(texte[i]);
+            }
+            cout << "phrase en MAJ: "<< endl;
+            cout << texte << endl;
+            break;
+        case 7:
+            system("clear");
+            display_menu();
+            for (int i = 0; i < texte.length(); i++)
+            {
+                texte[i] = tolower(texte[i]);
+            }
+            cout << "phrase en MIN: "<< endl;
+            cout << texte << endl;
+            break;
+        default:
+            break;
+        }
+    }
+
+}
