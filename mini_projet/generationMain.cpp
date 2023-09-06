b8f6efcbbf4b4faf12fdd5762f6a905584070dc5
stanou <stan.demontmarin@gmail.com>
2023-09-06 11:07:52 +0200

    clean generationMain.cpp

--- mini_projet/generationMain.cpp
+++ mini_projet/generationMain.cpp
@@ -1,22 +1,26 @@
 #include "lib.h"
 
-void generationMain(string nom_fichier,vector<string> &attributes){
+void generationMain(string nom_fichier, vector<string> &attributes) { 
     
     ofstream fichier3("main.cpp");
-    if(fichier3.is_open()){
-        fichier3<< "#include " << "\"" << nom_fichier 
+
+    if(fichier3.is_open()) {
+        fichier3 << "#include " << "\"" << nom_fichier 
         << ".h\"" << '\n'
         << "#include <iostream>" << '\n'
         << '\n'
         << "int main() {" << '\n'
         << '\t' << firstMaj(nom_fichier) << " obj;\n";
-        for(string elem : attributes){
-            fichier3 << "\tobj.set" << firstMaj(elem) <<"(42);\n";
+
+        for(string elem : attributes) {
+            fichier3 << "\tobj.set" << firstMaj(elem) << "(42);\n";
         }
-        for(string elem : attributes){
+
+        for(string elem : attributes) {
             fichier3 <<"\tstd::cout << " << "\"" << firstMaj(elem)
             << " = \" << obj.get" << firstMaj(elem) << "() << std::endl;" << '\n';
         }
+        
         fichier3 << '\n' <<"return 0;\n}";            
         fichier3.close();
     }
