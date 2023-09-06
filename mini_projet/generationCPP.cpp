7d55294916a3b098b9aa59caafea9db09b097dd1
stanou <stan.demontmarin@gmail.com>
2023-09-06 16:06:54 +0200

    cleaned files

--- mini_projet/generationCPP.cpp
+++ mini_projet/generationCPP.cpp
@@ -6,11 +6,14 @@ void generationCPP(string nom_fichier, vector<string> &attributes, vector<string
     regex notType("^(?!.*\\b(vector|string|int|bool|float|double|char|null|const|static|volatile|inline|void|short|long|signed|unsigned|struct|union|class|enum|virtual|override|final|public|protected|private)\\b).*$");
     regex type("^(string|int|bool|float|double|char|void|short|int|long|unsigned|signed|wchar_t|char16_t|char32_t|char8_t)$");
     regex accent(".*[àáâãäåçèéêëìíîïðòóôõöùúûüýÿÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÐÒÓÔÕÖÙÚÛÜÝ].*");
+    
     string choix;
     string attribute;
-    int compteur_err=1;
+    
+    int compteur_err = 1;
     int compteur = 0;
     int index = 0;
+    
     ofstream fichier1(nom_fichier+".cpp"); 
 
     if(fichier1.is_open()) {
@@ -31,18 +34,15 @@ void generationCPP(string nom_fichier, vector<string> &attributes, vector<string
                         attributes.push_back(attribute);
                         cin.clear();
                         cin.ignore();
-                    } 
-                    else if(contientAccent(attribute)) {
+                    } else if (contientAccent(attribute)) {
                         cout << "La variable numero : " << compteur_err << " contient un accent ! (cette variable est donc effacé)" << endl;
                         cin.clear();
                         cin.ignore();
-                    }
-                    else if(notExist(attribute,attributes)) {
+                    } else if(notExist(attribute,attributes)) {
                         cout << "La variable : " << attribute << " a une mauvaise syntaxe - Variable non créée" << endl;
                         cin.clear();
                         cin.ignore();
-                    }                  
-                    else{
+                    } else {
                         cout << "La variable : " << attribute << " est déjà existante- Variable non créée" << endl;
                         cin.clear();
                         cin.ignore();
@@ -58,35 +58,33 @@ void generationCPP(string nom_fichier, vector<string> &attributes, vector<string
                 
                     // Cas ou l'on saisie un type
                     if(compteur % 2 == 0 ){
-                        if(regex_match(attribute, type)){
+                        if(regex_match(attribute, type)) {
                             monType.push_back(attribute);                      
                             cin.clear();
                             cin.ignore();
-                            }
-                        else
-                            cout << "Le type de : " << attribute << " n'est pas un type de variable existant" << endl;                      
+                        } else {
+                            cout << "Le type de : " << attribute << " n'est pas un type de variable existant" << endl;
+                        }
                         compteur++;
                         continue;
                     }
+
                     // Cas ou l'on saisie une variable
-                    if(compteur % 2 == 1 ){
+                    if(compteur % 2 == 1 ) {
                         if(regex_match(attribute, variableName) && regex_match(attribute, notType) && notExist(attribute,attributes)) {
                             attributes.push_back(attribute);
                             cin.clear();
                             cin.ignore();
                             compteur++;
-                            }
-                            else if(contientAccent(attribute)) {
+                        } else if(contientAccent(attribute)) {
                         cout << "La variable numero : " << compteur_err << " contient un accent ! (cette variable est donc effacé)" << endl;
                         cin.clear();
                         cin.ignore();
-                    }
-                    else if(notExist(attribute,attributes)) {
+                    } else if(notExist(attribute,attributes)) {
                         cout << "La variable : " << attribute << " a une mauvaise syntaxe - Variable non créée" << endl;
                         cin.clear();
                         cin.ignore();
-                    }                  
-                    else{
+                    } else {
                         cout << "La variable : " << attribute << " est déjà existante- Variable non créée" << endl;
                         cin.clear();
                         cin.ignore();
@@ -94,34 +92,34 @@ void generationCPP(string nom_fichier, vector<string> &attributes, vector<string
                     compteur_err++;
                     }
                 }
-
                 break;
             }
-            
         }
     }
-        for(string elem : attributes) {
-            fichier1 << "\t" << elem << " = " << 0 << ";" << endl;
-        }
-        fichier1 << "}" << endl;
-        fichier1 << firstMaj(nom_fichier) << "::" << firstMaj(nom_fichier) << "(";
-        
-        for(string elem: attributes) { 
-            if(attributes.back()!= elem) {
-                fichier1 << monType[index]  << " " << elem << ", " ;
-            } else {
-                fichier1 << monType[index] << " " <<  elem << ") {" << endl;
-            }
-            index++;
-        }
 
-        for(string elem : attributes) {
-            fichier1 << "\t this->" << elem << " = "<< elem << ";" << endl;
-        }
-
-        fichier1 << "}" << endl;
-        createGetters(&fichier1, attributes, firstMaj(nom_fichier), monType);
-        createSetters(&fichier1, attributes, firstMaj(nom_fichier), monType);
+    for(string elem : attributes) {
+        fichier1 << "\t" << elem << " = " << 0 << ";" << endl;
+    }
+    fichier1 << "}" << endl;
+    fichier1 << firstMaj(nom_fichier) << "::" << firstMaj(nom_fichier) << "(";
+    
+    for(string elem: attributes) { 
         
-        fichier1.close();
+        if(attributes.back()!= elem) {
+            fichier1 << monType[index]  << " " << elem << ", " ;
+        } else {
+            fichier1 << monType[index] << " " <<  elem << ") {" << endl;
+        }
+        index++;
+    }
+
+    for(string elem : attributes) {
+        fichier1 << "\t this->" << elem << " = "<< elem << ";" << endl;
+    }
+
+    fichier1 << "}" << endl;
+    createGetters(&fichier1, attributes, firstMaj(nom_fichier), monType);
+    createSetters(&fichier1, attributes, firstMaj(nom_fichier), monType);
+    
+    fichier1.close();
 }
